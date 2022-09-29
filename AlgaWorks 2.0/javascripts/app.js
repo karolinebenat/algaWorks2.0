468eeda1316f24fcf8f4bb018a17cc146ad7960d
karolinebenat <68245795+karolinebenat@users.noreply.github.com>
2022-09-29 17:01:25 -0300

    Add files via upload

---
+++ AlgaWorks 2.0/javascripts/app.js
@@ -0,0 +1,13 @@
+var alternador = document.querySelector('.js-botao-chaveador');
+
+var menu = document.querySelector('.js-menu');
+    menu.classList.toggle('esta-exibindo')
+
+
+alternador.onclick = function(){
+
+    var menu = document.querySelector('.js-menu');
+    menu.classList.toggle('esta-exibindo')
+
+
+}
